9c38ff72701ed36585084a59c6f3077240d1c147
quysterben <quysterben@gmail.com>
2023-08-31 21:54:13 +0700

    feat/ui: add toast register page validation

--- public/src/pages/Register.js
+++ public/src/pages/Register.js
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react'
-
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 
-import styles from './Register.module.css'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
+import styles from './Register.module.css'
 import { BsChatHeart } from "react-icons/bs";
 
 export default function Register() {
@@ -12,18 +13,60 @@ export default function Register() {
     const passwordData = useRef();
     const rePasswordData = useRef();
 
+    const toastOptions = {
+        position: 'bottom-right',
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: 'dark'
+    }
+
     const handleSignupBtn = (e) => {
         e.preventDefault();
-        const data = {
-            username: usernameData.current.value,
-            email: emailData.current.value,
-            password: passwordData.current.value,
-            repassword: rePasswordData.current.value
+        if (handleValidation()) {
+            const data = {
+                username: usernameData.current.value,
+                email: emailData.current.value,
+                password: passwordData.current.value,
+                repassword: rePasswordData.current.value
+            }
+            console.log(data);
         }
     }
 
+    const handleValidation = () => {
+        if (usernameData.current.value.length < 3) {
+            toast.error(
+                "Username should be greater than 3 characters.",
+                toastOptions
+            );
+            return false;
+        } else if (emailData.current.value === '') {
+            toast.error(
+                "Email is required.",
+                toastOptions
+            );
+            return false;
+        } else if (passwordData.current.value.length < 8) {
+            toast.error(
+                "Password should be greater than 8 characters.",
+                toastOptions
+            );
+            return false;
+        } else if (passwordData.current.value !== rePasswordData.current.value) {
+            toast.error(
+                "Confirm password does not match.",
+                toastOptions
+            );
+            return false;
+        }
+
+        return true;
+    };
+
     return (
         <div className={styles.container}>
+            <ToastContainer />
             <div className={styles.form}>
                 <div className={styles.title}>
                     <BsChatHeart size={34} className={styles.icon} />
